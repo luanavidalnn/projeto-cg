438e05bc8a4ea26b330530e364bdfe72a5c74a31
Luana <luanadevops@gmail.com>
2023-08-28 10:21:28 -0300

    include xmax,min and ymax,min

--- app.py
+++ app.py
@@ -18,8 +18,13 @@ def on_connect():
 @socketio.on('change_coordinates')
 def change_coordinates(data):
     global coordinates
-    coordinates['x'] = data['x']
-    coordinates['y'] = data['y']
+    coordinates['xMax'] = data['xMax']
+    coordinates['xMin'] = data['xMin']
+    coordinates['yMax'] = data['yMax']
+    coordinates['yMin'] = data['yMin']
+    coordinates['x'] = data['xMax'] - data['xMin']
+    coordinates['y'] = data['yMax'] - data['yMin']
+
     emit('update_coordinates', coordinates, broadcast=True)
 
 if __name__ == '__main__':
